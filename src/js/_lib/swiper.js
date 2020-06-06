ae81d8aa11307e034768e179fe2d7aaeee4b7be9
Andrii <Luckyhidalgo@gmail.com>
2020-06-06 12:23:40 +0300

    Changed to full-width. Reduced footer nav & copyright font size. Centered header view text. Reduced logo size.

--- src/js/_lib/swiper.js
+++ src/js/_lib/swiper.js
@@ -24,7 +24,7 @@ const initSwiper = () => {
 			//
 			slidesPerView: 'auto',
 			spaceBetween: 0,
-			slidesPerGroup: 5,
+			slidesPerGroup: 3,
 			navigation: {
 				nextEl: sliderArrow + ' .list__arrow--next',
 				prevEl: sliderArrow + ' .list__arrow--prev',
