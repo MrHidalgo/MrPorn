b5cd4bc52838ca229a6534bc393ec9f3dca3bad7
Ishan Jayman <gt2ishan@gmail.com>
2022-01-04 08:53:56 +0530

    live theme footer fixes for light theme.

--- dest/js/app.js
+++ dest/js/app.js
@@ -1824,7 +1824,7 @@ var isCategoriesRendered = false;
     		if(!isCategoriesRendered){
     			isCategoriesRendered = true;
     			renderAllOtherCategories();
-    			}
+    				}
     	}
     }*/
 
