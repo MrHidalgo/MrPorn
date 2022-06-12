5d6f132304e7df3e9cdb5396d309cc02b80428b0
Ishan Jayman <gt2ishan@gmail.com>
2022-06-12 16:53:49 +0530

    merging review 3 col with no sneak

--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -170,6 +170,38 @@ function verifyCookie(){
 	}
 }
 
+function setInnerHeight(){
+	let vh = window.innerHeight;
+	let deviceHeight = window.innerHeight;
+	let keyboardHeight = 0;
+
+	if(window.visualViewport){
+		vh = window.visualViewport.height;
+	}
+	keyboardHeight = deviceHeight - vh;
+
+	if(keyboardHeight>0){
+		keyboardHeight += 100;
+	}
+
+	document.documentElement.style.setProperty('--kh', `${keyboardHeight}px`);
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+	let wInnerHeight = window.innerHeight;
+	document.documentElement.style.setProperty('--wih', `${wInnerHeight}px`);
+}
+
+function preventDefault(e){
+	e.preventDefault();
+}
+
+function disableScroll(){
+	document.body.addEventListener('touchmove', preventDefault, { passive: false });
+}
+function enableScroll(){
+	document.body.removeEventListener('touchmove', preventDefault);
+}
+
 let isCategoriesRendered = false;
 let lastMobileSimilarSite;
 
@@ -918,28 +950,17 @@ let lastMobileSimilarSite;
 
 			window.addEventListener('resize', () => {
 				wInnerWidth = window.innerWidth;
-				/*headerHeight = document.querySelector('#header').getBoundingClientRect().height;
-				if (document.body.classList.contains('home')) {
-					let vh = window.innerHeight * 0.01;
-					document.documentElement.style.setProperty('--vh', `${vh}px`);
-				}*/
+				headerHeight = document.querySelector('#header').getBoundingClientRect().height;
 
-				if(wInnerWidth > 1023) {
-					if(document.querySelector('.list__specification.is-open')) {
-						document.getElementsByTagName('html')[0].classList.remove('is-hideScroll');
-						document.getElementsByTagName('body')[0].classList.remove('is-hideScroll');
-					}
-				} else {
-					if(!isMobileOrTablet){
-						if(document.querySelector('.list__specification.is-open')) {
-							document.getElementsByTagName('html')[0].classList.add('is-hideScroll');
-							document.getElementsByTagName('body')[0].classList.add('is-hideScroll');
-						}
-					}
-
-
-				}
+				setInnerHeight();
 			});
+
+			if (window.visualViewport) {
+				window.visualViewport.addEventListener("resize", () => {
+					setInnerHeight();
+					console.log("visualViewport RESIZE IS TRIGGERED");
+				});
+			}
 
 		});
 	}
