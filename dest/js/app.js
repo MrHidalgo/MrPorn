1e335765b7d559ea9d27fa80bbf0879c99a5e903
ishanjayman <gt2ishan@gmail.com>
2025-05-28 10:39:34 +0530

    Category tags implementation

--- dest/js/app.js
+++ dest/js/app.js
@@ -1513,6 +1513,21 @@ function loadJS(url, implementationCode, location) {
 }
 
 ;
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleButton = document.querySelector('.dropdown-toggle');
+  var dropdownMenu = document.querySelector('.dropdown-menu');
+  toggleButton.addEventListener('click', function () {
+    dropdownMenu.classList.toggle('show');
+    toggleButton.classList.toggle('show'); // Add 'show' class to the button for icon rotation
+  }); // Close the dropdown if clicked outside
+
+  document.addEventListener('click', function (event) {
+    if (!toggleButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+      dropdownMenu.classList.remove('show');
+      toggleButton.classList.remove('show'); // Remove 'show' class from the button
+    }
+  });
+});
 
 (function () {
   var FX = {
